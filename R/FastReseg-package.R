fda309984cddf6dc2a1f675d6fc83ba1f181c9ce
David Ross <dross@nanostring.com>
2022-08-11 10:18:43 -0700

    Use data.table throughout package

--- R/FastReseg-package.R
+++ R/FastReseg-package.R
@@ -1,6 +1,9 @@
 #' @keywords internal
 "_PACKAGE"
 
+# Make sure data.table knows we know we're using it
+.datatable.aware = TRUE
+
 # The following block is used by usethis to automatically manage
 # roxygen namespace tags. Modify with care!
 ## usethis namespace: start
